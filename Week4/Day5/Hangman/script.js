67476f228f3084c9e02e62c3157456e669b30df0
Arthurgit cinfig --global user.email arthurr.ie@gmail.com <arthurr.ie@gmail.com>
2020-05-15 14:52:27 +0300

    hangman Revised

--- Week4/Day5/Hangman/script.js
+++ Week4/Day5/Hangman/script.js
@@ -1,52 +1,66 @@
-let word = prompt("Player 1 please choose 1 word").split("");
-let guess_word =("*".repeat(word.length)).split("");
-let used_letters=[];
-
+let word = [];
+let guessed_letters=0;
 let lives = 10;
-console.log("let the game begin");
-console.log("lives left: "+lives);
-console.log(guess_word.join(""));
+word = prompt(" please choose sentance(up to 20 charecters)").split("");
+while(word.length>20 ){
+  word = prompt(" please choose sentance(up to 20 charecters)").split("");
+}
+guessed_letters = (word.join("")).split(" ").join("").length;
+console.log(guessed_letters);
 
+createWord(word);
 
-while(lives>0){
-  var letter = prompt("Choose a letter");
-
-  if(used_letters.includes(letter)){
-    console.log("this letter was used");
-    console.log("your used letters are: "+ used_letters.join(","));
-    console.log(guess_word.join(""));
-  }
-  else if( word.includes(letter)){
-            for(var i=0; i<word.length;i++){
-                if(word[i]==letter){
-                  guess_word[i] = letter;
-                }
-            }
-
-            console.log("that was a correct letter, good job");
-            console.log("your number of lives is: "+lives);
-            used_letters.push(letter);
-            console.log("your used letters are: "+ used_letters.join(","));
-            console.log(guess_word.join(""));
-            if(guess_word.join("") === word.join("")){
-              break;
-            }
-
+function my_f(letter){
+    if(word.includes(letter)==false){
+      document.querySelectorAll("."+letter)[0].disabled=true;
+      loselife(document.querySelectorAll("#l-"+lives)[0]);
+      // document.querySelectorAll("#l-"+lives)[0].style.color="black";
+        lives-=1;
+      if(lives ==0 ){
+        setTimeout(alertFunc("you lost :( \r\n the word was: "+word.join("") +"\r\n refresh window to start new game" ),3);
+      }
         }
       else{
-            console.log("Wrong letter");
-            lives -=1;
-            console.log("you lost a life, now it is:" + lives);
-            used_letters.push(letter);
-            console.log("your used letters are: "+ used_letters.join(","));
-            console.log(guess_word.join(""));
+        for (var elem of document.querySelectorAll(".h-"+letter)){
+                  elem.innerHTML=letter;
+                  guessed_letters-=1;
+        }
+        document.querySelectorAll("."+letter)[0].disabled=true;
 
-          }
+        if(guessed_letters==0){
+          setTimeout(alertFunc("you have won!!! \r\n refresh window for new game"), 1)
+        }
+      }
 }
-if(lives>0){
-    console.log("you have won!");
+function alertFunc(msg){
+  alert(msg);
 }
-else{
-  console.log("sorry, but you are out of lives");
-  console.log("the word was: "+ word.join(""));
+
+function loselife(elem){
+
+  for (let i = 4;i>=0;i--)
+  {
+    elem.setAttribute("style","transform:translateY(100px);transition: all 1s ease-in-out;");
+    elem.style.opacity=i/4
+
+  }
+}
+
+function createWord(arr){
+
+  for (x of arr){
+
+      var elem=document.createElement('span');
+      if (x ==" "){
+          elem.innerHTML=" ";
+      }
+      else{
+        elem.innerHTML="_";
+      }
+        elem.setAttribute("style","margin:auto;");
+        elem.setAttribute("class","h-"+x);
+        if(arr.length <10){ elem.style.fontSize="3em";}
+        else if(arr.length <20){ elem.style.fontSize="2em";}
+        document.querySelectorAll(".guess")[0].appendChild(elem);
+  }
 }
