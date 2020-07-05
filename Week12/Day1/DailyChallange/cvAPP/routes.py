b40d098922dc792dc58168cb88c3c4c8efda8fc9
Arthurgit cinfig --global user.email arthurr.ie@gmail.com <arthurr.ie@gmail.com>
2020-07-05 23:51:02 +0300

    day 1 daily

---
+++ Week12/Day1/DailyChallange/cvAPP/routes.py
@@ -0,0 +1,22 @@
+import flask
+from flask import request, redirect
+
+from cvAPP import app
+
+
+@app.route("/")
+def index():
+    return flask.render_template("cvform.html")
+
+
+@app.route("/submit-cv", methods=["GET", "POST"])
+def build_cv():
+
+    if request.method == "POST":
+       if request.form['template'] == "1":
+           return flask.render_template("cvoutput1.html", cv=request.form)
+       if request.form['template'] == "2":
+           return flask.render_template("cvoutput2.html", cv=request.form)
+
+
+    return flask.render_template("cvform.html")
